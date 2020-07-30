5f3c5953e094773a8079387d7d976581fe5667f1
刘佳昌-2016 <1040496263@qq.com>
2020-07-30 16:01:40 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+# -*- coding:utf-8 -*-
+
+
+from setuptools import setup, find_packages
+
+setup(
+
+    name="demo",
+
+    version="0.1",
+
+    url='https://github.com/liujiachang/testsdk.git',
+
+    long_description=open('README.md').read(),
+
+    packages=find_packages(),
+
+)
